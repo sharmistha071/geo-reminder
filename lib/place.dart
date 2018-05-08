884292a9c3e5052958078bef55a9966a178975d4
sharmistha071 <shormy.roy@gmail.com>
2018-05-09 04:00:58 +0600

    place autocomplete search added

--- lib/place.dart
+++ lib/place.dart
@@ -3,87 +3,15 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_google_places_autocomplete/flutter_google_places_autocomplete.dart';
 
+import 'main.dart';
+import 'addTodo.dart';
+
 const kGoogleApiKey = "AIzaSyDEtGrV-U7k4_LY52k-u9ScyQeEIvR5f7k";
 
 GoogleMapsPlaces _places = new GoogleMapsPlaces(kGoogleApiKey);
 
-main() {
-  runApp(new MaterialApp(
-    title: "My App",
-    theme: new ThemeData(accentColor: Colors.redAccent),
-    routes: {
-      "/": (_) => new MyApp(),
-      "/search": (_) => new CustomSearchScaffold()
-    },
-  ));
-}
-
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => new _MyAppState();
-}
-
-final homeScaffoldKey = new GlobalKey<ScaffoldState>();
 final searchScaffoldKey = new GlobalKey<ScaffoldState>();
 
-class _MyAppState extends State<MyApp> {
-  Mode _mode = Mode.overlay;
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      key: homeScaffoldKey,
-      appBar: new AppBar(
-        title: new Text("My App"),
-      ),
-      body: new Center(
-          child: new Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          new DropdownButton(
-              value: _mode,
-              items: <DropdownMenuItem<Mode>>[
-                new DropdownMenuItem<Mode>(
-                    child: new Text("Overlay"), value: Mode.overlay),
-                new DropdownMenuItem<Mode>(
-                    child: new Text("Fullscreen"), value: Mode.fullscreen),
-              ],
-              onChanged: (m) {
-                setState(() {
-                  _mode = m;
-                });
-              }),
-          new RaisedButton(
-              onPressed: () async {
-                // show input autocomplete with selected mode
-                // then get the Prediction selected
-                Prediction p = await showGooglePlacesAutocomplete(
-                    context: context,
-                    apiKey: kGoogleApiKey,
-                    onError: (res) {
-                      homeScaffoldKey.currentState.showSnackBar(
-                          new SnackBar(content: new Text(res.errorMessage)));
-                    },
-                    mode: _mode,
-                    language: "en",
-                    // components: [new Component(Component.country, "bn")]
-                  );
-
-                displayPrediction(p, homeScaffoldKey.currentState);
-              },
-              child: new Text("Search places")),
-          new RaisedButton(
-            child: new Text("Custom"),
-            onPressed: () {
-              Navigator.of(context).pushNamed("/search");
-            },
-          ),
-        ],
-      )),
-    );
-  }
-}
 
 Future<Null> displayPrediction(Prediction p, ScaffoldState scaffold) async {
   if (p != null) {
@@ -95,8 +23,6 @@ Future<Null> displayPrediction(Prediction p, ScaffoldState scaffold) async {
     scaffold.showSnackBar(
         new SnackBar(content: new Text("${p.description} - $lat/$lng"))
     );
-    print('$lat');
-    print('$ln');
   }
 }
 
@@ -108,7 +34,6 @@ class CustomSearchScaffold extends GooglePlacesAutocompleteWidget {
       : super(
             apiKey: kGoogleApiKey,
             language: "en",
-            // components: [new Component(Component.country, "us")]
           );
 
   @override
@@ -118,9 +43,16 @@ class CustomSearchScaffold extends GooglePlacesAutocompleteWidget {
 class _CustomSearchScaffoldState extends GooglePlacesAutocompleteState {
   @override
   Widget build(BuildContext context) {
-    final appBar = new AppBar(title: new AppBarPlacesAutoCompleteTextField());
+    final appBar = new AppBar(
+      title: new AppBarPlacesAutoCompleteTextField(),
+      backgroundColor: Colors.redAccent[400],
+    );
     final body = new GooglePlacesAutocompleteResult(onTap: (p) {
       displayPrediction(p, searchScaffoldKey.currentState);
+      Navigator.push(
+        context,
+        new MaterialPageRoute(builder: (context) => new AddTodo()),
+      );
     });
     return new Scaffold(key: searchScaffoldKey, appBar: appBar, body: body);
   }
